46add295d5e646b5886b4309b1cde3e4bd8d0cac
Akashnule17 <85895750+Akashnule17@users.noreply.github.com>
2021-06-15 09:04:47 +0530

    Add files via upload

---
+++ june 14-06-2021/factorial.c
@@ -0,0 +1,19 @@
+/* factorial program */
+
+#include<stdio.h>   // standard for getting input and output 
+
+int main()  
+ {
+ //program statements starts here 
+   int n=6,result=1;     // declaring two "int" variables as "n" and "result" 
+   while(n!=0)          // checking the condition equals to zero if yes go to output 
+   {
+     result = result*n;      //multipling the result with n 
+     n--;                   //post decrementing 
+   }
+      
+       /*output*/
+  printf("%d\n",result);        // printing the factorial result 
+  return 0;                    // returing the integer value to zero 
+  
+ }        
